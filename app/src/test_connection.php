e55d82b83bd9029b52c1f0891e9c87f6f5394b06
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-09-11 22:06:12 +0900

    Directory modification

--- app/src/test_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:dbname=test_db;host=php-db;charset=utf8', 'test', 'test');
-    echo '接続成功';
-} catch (PDOException $e) {
-    echo '接続失敗: ' . $e->getMessage();
-}
-?>
